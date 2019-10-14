11cb5f515bcb39c6e1bc63be481d7ba1afe8e571
Edgar Rebane <erebane@hajaan.nu>
2019-10-14 11:45:01 +0300

    Add virtualbox install script

---
+++ scripts/install-virtualbox.sh
@@ -0,0 +1,2 @@
+wget https://download.virtualbox.org/virtualbox/6.0.12/virtualbox-6.0_6.0.12-133076~Ubuntu~bionic_amd64.deb
+sudo dpkg -i virtualbox-6.0_6.0.12-133076~Ubuntu~bionic_amd64.deb && rm virtualbox-6.0_6.0.12-133076~Ubuntu~bionic_amd64.deb
