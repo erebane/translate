b7737902c7455d273bef6de33ff652fe2e94c644
Edgar Rebane <erebane@hajaan.nu>
2019-10-14 08:54:10 +0300

    Remove obsolete url reference, minor changes to comments

--- scripts/delete-pootle.sh
+++ scripts/delete-pootle.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 #
-# Remove https://translate.heikkihakala.fi pootle
+# Remove pootle installation db and folders
 #
-# 1. Remove postgresql db pootle
-# 2. Remove postgres db user pootle
-# 3. Remove installation folder /opt/pootle/
-# 4. Remove system user pootle and its home
+# - Remove postgresql db pootle
+# - Remove postgres db user pootle
+# - Remove installation folder /opt/pootle/
+# - Remove system user pootle and its home
 #
 
 output_error="Pootle remove ERROR: "
 output_ok="Pootle remove OK: "
 output_end=""
 
-# 1. Remove db pootle
+# Remove db pootle
 sudo su postgres -c "dropdb pootle"
 
-# 2. Remove postgres db user pootle
+# Remove postgres db user pootle
 sudo su postgres -c "dropuser pootle"
 
-# 3. Remove installation folder /opt/pootle/
+# Remove installation folder /opt/pootle/
 sudo rm -rf /opt/pootle
 sudo rm -rf /opt/pootle-projects
 
-# 4. Remove system user pootle and its home
+# Remove system user pootle and its home
 sudo deluser --remove-home pootle
